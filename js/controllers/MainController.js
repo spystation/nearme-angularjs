f148093a6fe8ee9a67ccee384ff4575a804851e8
spystation <charles@spystation.com>
2015-03-01 14:33:47 -0500

    Add Leaflet as per Near-Me-Part 2

---
+++ js/controllers/MainController.js
@@ -0,0 +1,27 @@
+app.controller('MainController', ['$scope', 'places', function($scope, places){
+
+  $scope.MapCenter = {
+    lat: 40.741934,
+    lng: -74.004897,
+    zoom: 17
+  };
+
+  places.success(function(data) {
+  	$scope.geodata = data;
+    $scope.mapMarkers = geodataToMarkers($scope.geodata);
+  });
+
+  // removed in Part 2 --- $scope.mapMarkers = [
+  //  {
+  //    lat: 40.741389,
+  //    lng: -74.003056,
+  //    message: "111 Eighth Avenue"
+  //  },
+  //  {
+  //    lat: 40.7425,
+  //    lng: -74.006111,
+  //    message: "Chelsea Market"
+  //  }
+  //]
+
+}]);
